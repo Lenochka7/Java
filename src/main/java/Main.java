4cada318cf6f762c3a756008039e5d796380b1d3
Elena Komolova <malen-01@mail.ru>
2022-12-10 21:07:41 +0700

    yes

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 20000;
+        int mile = 20;
+        int miles = ticket / mile;
+   System.out.println("Итого бонусных миль: " + miles); }
+}
